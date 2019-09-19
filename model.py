a344fa94ec319c3c6fc88bc276db354dc0c462a9
Silvio Mori Neto <silvio@technomori.com>
2019-09-19 18:56:44 -0300

    feat: Create a simple QNetwork model

---
+++ model.py
@@ -0,0 +1,27 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class QNetwork(nn.Module):
+    """Actor (Policy) Model."""
+
+    def __init__(self, state_size, action_size, seed, hidden_layer_1=512, hidden_layer_2=512):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+        """
+        super(QNetwork, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        
+        self.fc1 = nn.Linear(state_size, hidden_layer_1)
+        self.fc2 = nn.Linear(hidden_layer_1, hidden_layer_2)
+        self.fc3 = nn.Linear(hidden_layer_2, action_size)
+        
+    def forward(self, state):
+        """Build a network that maps state -> action values."""
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        return self.fc3(x)
