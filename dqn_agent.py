fdd89ef168c9ab195935114a802fb42325e8b6c6
Silvio Mori Neto <silvio@technomori.com>
2019-10-21 10:38:05 -0300

    feat: Adjust hyperparameters: solve in 21 episodes

--- dqn_agent.py
+++ dqn_agent.py
@@ -15,12 +15,12 @@ BUFFER_SIZE = int(1e5)  # replay buffer size
 BATCH_SIZE = 64         # minibatch size
 GAMMA = 0.99            # discount factor
 TAU = 1e-2              # for soft update of target parameters
-LR = 5e-4               # learning rate 
+LR = 4.8e-4               # learning rate 
 UPDATE_EVERY = 4        # how often to update the network
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-USE_DOUBLE_DQN = False
+USE_DOUBLE_DQN = True
 USE_PRIORITIZED_REPLAY = False
 USE_DUELING_NETWORK = True
 
